85e941edf64e7a49db360cbcb00bc9befdf00b4e
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2020-11-08 05:53:06 +0800

    fix: changes url and  the way what and how to get data

--- carinama/spiders/kamusnama.py
+++ carinama/spiders/kamusnama.py
@@ -1,74 +1,21 @@
 import scrapy
 
 
-
-class KamusNamaSpider(scrapy.Spider):
-    name = 'kamusnama_v1'
+class KamusNamaLakiSpider(scrapy.Spider):
+    name = 'kamus_nama_laki'
     protocol = 'https://'
     base_url = 'kamusnama.com/'
     start_urls = [
         protocol + base_url + 'cari-nama-laki-laki?page=1',
-        protocol + base_url + 'cari-nama-perempuan?page=1'
-    ]
-        
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-    
-    def parse(self, response):
-        results = response.css('div.search-result-item')
-        
-        for result in results:
-            name = result.css('h4 > a::text').extract_first()
-            desc = result.css('p *::text').extract()
-            desc = " ".join(desc)
-            
-            gender = result.css('p > strong::text').extract_first().upper()
-            lang = None
-            source = response.url
-            # print(description)
-            
-            yield {
-                'name': name,
-                'desc': desc,
-                'gender': gender,
-                'language': lang,
-                'url': source
-            }
-        
-        next_page_url = response.css("li.page-item > a[rel='next']::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
-            
-    
-
-class KamusNamaSpider(scrapy.Spider):
-    name = 'kamusnama_v2'
-    protocol = 'https://'
-    base_url = 'kamusnama.com/'
-    start_urls = [
-        protocol + base_url + 'cari-nama-laki-laki?page=1',
-        protocol + base_url + 'cari-nama-perempuan?page=1'
     ]
     
-    search_detail = [
-        'rangkaian-nama-depan',
-        'rangkaian-nama-tengah',
-        'rangkaian-nama-belakang'
-    ]
+    search_detail = ['rangkaian-nama-depan',]
     
-    temp = []
     
     def build_start_detail_url(self, name):
-        urls = []
-        sub_name = []
-        for sub in self.search_detail:
-            sn = '_'.join(sub.split('-')[1:])
-            sub_name.append(sn)
-            
-            url = self.protocol + self.base_url + sub + '/' + name + '?page=1'
-            urls.append(url)
-        return urls, sub_name
+        sub = self.search_detail[0]
+        url = self.protocol + self.base_url + sub + '/' + name + '?page=1'
+        return url
         
     def start_requests(self):
         for url in self.start_urls:
@@ -79,39 +26,112 @@ class KamusNamaSpider(scrapy.Spider):
         
         for result in results:
             name = result.css('h4 > a::text').extract_first().strip()
-            desc = result.css('p *::text').extract()
-            desc = " ".join(desc)
             gender = result.css('p > strong::text').extract_first().upper()
-            source = response.url
-            # print(description)
-            
+
             data =  {
                 'base_name': name,
-                'desc': None,
                 'gender': gender,
-                'language': None,
-                'base_url': source
             }
             
-            detail_url, sub_name = self.build_start_detail_url(name)
-            for dl_url, sn in zip(detail_url, sub_name):
-                yield scrapy.Request(url=dl_url, callback=self.detail_parse,
-                                     cb_kwargs=dict(parse_data=data, sub_name=sn))
+            detail_url = self.build_start_detail_url(name)
+            yield scrapy.Request(url=detail_url, callback=self.detail_parse, cb_kwargs=dict(parse_data=data))
         
         next_page_url = response.css("li.page-item > a[rel='next']::attr(href)").extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
             
             
-    def detail_parse(self, response, parse_data, sub_name):
+    def detail_parse(self, response, parse_data):
         results = response.css('li.sc_list_item.icon-right-open-big *::text')
         for result in results:
             parse_data['name'] = result.extract()
-            parse_data['position'] = sub_name
-            parse_data['detail_url'] = response.url
+            parse_data['source'] = response.url
             yield parse_data
+            
+            
+class KamusNamaPerempuanSpider(scrapy.Spider):
+    name = 'kamus_nama_perempuan'
+    protocol = 'https://'
+    base_url = 'kamusnama.com/'
+    start_urls = [
+        protocol + base_url + 'cari-nama-perempuan?page=1'
+    ]
+    
+    search_detail = ['rangkaian-nama-depan',]
+    
+    
+    def build_start_detail_url(self, name):
+        sub = self.search_detail[0]
+        url = self.protocol + self.base_url + sub + '/' + name + '?page=1'
+        return url
+        
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        results = response.css('div.search-result-item')
+        
+        for result in results:
+            name = result.css('h4 > a::text').extract_first().strip()
+            gender = result.css('p > strong::text').extract_first().upper()
+
+            data =  {
+                'base_name': name,
+                'gender': gender,
+            }
+            
+            detail_url = self.build_start_detail_url(name)
+            yield scrapy.Request(url=detail_url, callback=self.detail_parse, cb_kwargs=dict(parse_data=data))
         
         next_page_url = response.css("li.page-item > a[rel='next']::attr(href)").extract_first()
         if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url), callback=self.detail_parse, 
-                                 cb_kwargs=dict(parse_data=parse_data, sub_name=sub_name))
+            yield scrapy.Request(response.urljoin(next_page_url))
+            
+            
+    def detail_parse(self, response, parse_data):
+        results = response.css('li.sc_list_item.icon-right-open-big *::text')
+        for result in results:
+            parse_data['name'] = result.extract()
+            parse_data['source'] = response.url
+            yield parse_data
+
+# class KamusNamaSpider(scrapy.Spider):
+#     name = 'kamusnama_v1'
+#     protocol = 'https://'
+#     base_url = 'kamusnama.com/'
+#     start_urls = [
+#         protocol + base_url + 'cari-nama-laki-laki?page=1',
+#         protocol + base_url + 'cari-nama-perempuan?page=1'
+#     ]
+        
+#     def start_requests(self):
+#         for url in self.start_urls:
+#             yield scrapy.Request(url=url, callback=self.parse)
+    
+#     def parse(self, response):
+#         results = response.css('div.search-result-item')
+        
+#         for result in results:
+#             name = result.css('h4 > a::text').extract_first()
+#             desc = result.css('p *::text').extract()
+#             desc = " ".join(desc)
+            
+#             gender = result.css('p > strong::text').extract_first().upper()
+#             lang = None
+#             source = response.url
+#             # print(description)
+            
+#             yield {
+#                 'name': name,
+#                 'desc': desc,
+#                 'gender': gender,
+#                 'language': lang,
+#                 'url': source
+#             }
+        
+#         next_page_url = response.css("li.page-item > a[rel='next']::attr(href)").extract_first()
+#         if next_page_url is not None:
+#             yield scrapy.Request(response.urljoin(next_page_url))
+            
+    
